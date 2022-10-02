5d3e95845ada39a37d6c80e59d2143264751248b
Methmi Nugawela <methmi.nuga@gmail.com>
2022-10-02 23:18:40 +0530

    Added program to parallelly calculate PI

---
+++ parallelization_with_openmp/calculate_pi_with parallel_programming.c
@@ -0,0 +1,40 @@
+// A program built with OpenMP that uses paralell programming to calculate a value for PI.
+// This program also outputs the average time taken for the prhram to run.
+// Please check out the README in this folder for the equation used
+
+#include <omp.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// Defines the number of times to run this program to calculate an average value for 
+// the time taken to run it
+#define RUNS 10
+
+int main(){
+
+    // n is a very large, arbitary integer
+	int n = 100000;
+
+	float value=0;
+    long int total = 0;
+    double tStart=0, tCalc=0, tStop=0;
+    double timeTotal = 0;
+    for (int s=1; s<=RUNS; s++){
+        total = 0;
+        tStart = omp_get_wtime();
+    #pragma omp parallel for reduction(+:value)
+        for (int a=1; a<=n; a++){
+            value = value + (1 / (1 + ((a-0.5)/n)*((a-0.5)/n)));
+        }
+        value = value/(float)n * 4;
+
+        printf("Pi =  %f\n", value);
+        tStop = omp_get_wtime();
+        tCalc = tStop - tStart;
+        printf("Time taken =  %f\n", tCalc);
+        if(s>3){
+            timeTotal += tCalc; 
+        }
+    }
+    printf("Average time taken =  %f\n", (float)timeTotal/RUNS);
+}
